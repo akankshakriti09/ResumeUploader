29ad47975e51cbed34df1fcc8c956f24bc7f387d
Akanksha Kriti <akankshakriti09@gmail.com>
2023-05-07 22:59:54 +0530

    material mui installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Grid, TextField, Typography, FormControlLabel, Checkbox, Button, Box, Alert, InputLabel, MenuItem, Select, FormControl, FormLabel,
+RadioGroup, Radio, FormGroup, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Avatar } 
+from '@mui/material';
+
 function App() {
   return (
     <>
-    <h1>Hello React</h1>
+      <Box display="flex" justifyContent="center" sx={{
+       backgroundColor:"error.light" , padding:2}}>
+        <Typography variant="h4" component="div" sx={{ 
+          fontweight:"bold", color:"white"}}>Resume Uploader</Typography>
+      </Box>
+      <Grid container justifyContent="center">
+        <Grid item xs={5}>
+          <h1>Form</h1>
+        </Grid>
+        <Grid item xs={7}>
+          <Box></Box>
+        </Grid>
+      </Grid>
     </>
   );
 }
