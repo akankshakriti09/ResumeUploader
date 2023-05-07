83933a2dc3fa1c7a3b294cad5b84fe0c5d8e465f
Akanksha Kriti <akankshakriti09@gmail.com>
2023-05-07 23:10:46 +0530

    created TableContainer

--- src/App.js
+++ src/App.js
@@ -23,6 +23,34 @@ function App() {
                 List of Candidates
               </Typography>
           </Box>
+
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">Name</TableCell>
+                  <TableCell align="center">Email</TableCell>
+                  <TableCell align="center">DOB</TableCell>
+                  <TableCell align="center">State</TableCell>
+                  <TableCell align="center">Gender</TableCell>
+                  <TableCell align="center">Location</TableCell>
+                  <TableCell align="center">Avatar</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell align="center">Sonam</TableCell>
+                  <TableCell align="center">sonam@gmail.com</TableCell>
+                  <TableCell align="center">20/10/1995</TableCell>
+                  <TableCell align="center">Bihar</TableCell>
+                  <TableCell align="center">Female</TableCell>
+                  <TableCell align="center">Delhi Ranchi</TableCell>
+                  <TableCell align="center"><Avatar src="#" /></TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+          
         </Grid>
       </Grid>
     </>
